db73ba49e31a64eee1a5b8b78b393f2505fe3c48
Jonathan Challinger <mr.challinger@gmail.com>
2016-08-04 14:54:40 -0700

    file write in thread

--- imagestream.py
+++ imagestream.py
@@ -2,28 +2,38 @@ import subprocess
 import os
 import shutil
 from collections import deque
+import multiprocessing as mp
+import itertools
+import threading
 
 imagename = os.path.expanduser('~/livestream/img.jpeg')
+streamname = '/dev/video0'
 
-avconv_cmd = ['avconv', '-i', '/dev/video0', '-f', 'image2pipe',  '-r', '1', '-vcodec', 'mjpeg', '-']
-avconv = subprocess.Popen(avconv_cmd, stdout = subprocess.PIPE, stderr = open(os.devnull,'wb'))
+def write_file(imagedata):
+    with open(imagename+'.tmp', 'wb') as f:
+        f.truncate()
+        f.write(imagedata)
+    shutil.move(imagename+'.tmp', imagename)
+
+avconv_cmd = ['avconv', '-max_delay', '1000', '-i', streamname, '-max_delay', '1000', '-f', 'image2pipe',  '-r', '1', '-vcodec', 'mjpeg', '-']
+avconv = subprocess.Popen(avconv_cmd, stdout = subprocess.PIPE)
 
 #jpeg_header = (0x00, 0x00, 0x00, 0x0C, 0x6A, 0x50, 0x20, 0x20, 0x0D, 0x0A, 0x87, 0x0A) # JPEG-2000
 jpeg_header = (0xFF, 0xD8) #JPEG 1998
 jpeg_footer = (0xFF, 0xD9)
-buf = deque(maxlen=max(len(jpeg_header), len(jpeg_footer)))
+buf = deque()
 
+worker=None
 while True:
     buf.append(ord(avconv.stdout.read(1)))
-    if tuple(buf) == jpeg_header[-len(jpeg_header):]:
-        # start of image
-        tmp = open(imagename+'.tmp', 'wb')
-        tmp.truncate()
-        tmp.write(''.join([chr(b) for b in buf]))
-        while tuple(buf)[-len(jpeg_footer):] != jpeg_footer:
-            buf.append(ord(avconv.stdout.read(1)))
-            tmp.write(chr(buf[-1]))
-        tmp.flush()
-        tmp.close()
+    while len(buf)>len(jpeg_header):
+        buf.popleft()
 
-        shutil.move(imagename+'.tmp', imagename)
+    if len(buf)>=len(jpeg_header) and tuple(itertools.islice(buf,len(buf)-len(jpeg_header),None)) == jpeg_header:
+        # start of image
+        while tuple(itertools.islice(buf,len(buf)-len(jpeg_footer),None)) != jpeg_footer:
+            buf.append(ord(avconv.stdout.read(1)))
+        if worker is None or not worker.isAlive():
+            worker = threading.Thread(target=write_file, args=(''.join([chr(b) for b in buf]),))
+            worker.start()
+        buf.clear()
